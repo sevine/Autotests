569be88ea7e9e9dd27e6e52d273358f9eedae058
Sergey Nemchuk <s.nemchuk@sdventures.com>
2019-05-28 18:47:05 +0300

    First commit

---
+++ Autotests/Tests/Test1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Autotests.Tests
+{
+    public class Test1
+    {
+        [Test]
+        public void HelloWorldTest()
+        {
+            Assert.That("Hello world".Length, Is.EqualTo(11));
+        }
+    }
+}
