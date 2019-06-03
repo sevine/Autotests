6a839f789affed8509e919fc817cb746e55cfe41
Sergey Nemchuk <s.nemchuk@sdventures.com>
2019-06-03 12:29:54 +0300

    first test

---
+++ Autotests/PageModel/IndexPage/Header.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Autotests.PageModel.IndexPage
+{
+    public class Header
+    {
+        public const string lnkShows = "a[href='/shows']";
+        public const string lnkEpisodes = "a[href='/video']";
+        public const string lnkSchedule = "a[href='/schedule']";
+        public const string lnkNewsAndSports = "//span[contains(text(), 'Sport')]/..";
+        public const string lnkShop = "//span[contains(text(), 'Shop')]/..";
+        public const string lnkApp = "a[href='/apps']";
+        public const string lnkSearch = "//button[@class='navigation__search navigation__item__title']";
+    }
+}
---
+++ Autotests/Tests/BasicTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using Autotests.PageModel.IndexPage;
+using OpenQA.Selenium;
+
+namespace Autotests.Tests
+{
+    [TestFixture]
+    public class BasicTests
+    {
+        const string nbcIndexPage = "https://www.nbc.com/";
+        ChromeDriver driver;
+
+        [SetUp]
+        public void Init()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(nbcIndexPage);
+            Thread.Sleep(5000);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void MainMenuNavigationTest()
+        {
+            Thread.Sleep(5000);
+        }
+    }
+}
